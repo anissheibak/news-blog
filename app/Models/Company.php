0099a3e6bcab6d1365e56a6fb971236dfbc31efc
anissheibak <sheibakanis@gmail.com>
2023-10-15 23:31:05 +0330

    company model completed

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Company extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+    protected $fillable=['title', 'envelope', 'image_path', 'location', 'email', 'website', 'phone', 'employees', 'established on', 'slug', 'tags', 'status'];
 }
